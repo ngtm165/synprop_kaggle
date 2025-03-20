c883ece91fcb1d5412a685590f6abfd41c8aea3c
ngtm165 <ngoctamtrannguyen123@gmail.com>
2025-03-20 07:01:25 +0700

    Update main_finetune.py

--- synprop/main_finetune.py
+++ synprop/main_finetune.py
@@ -22,12 +22,12 @@ if __name__ == "__main__":
     arg_parser.add_argument("--monitor_name", type=str, default="monitor.txt")
     arg_parser.add_argument("--Data_folder", type=str, default="./Data/")
     arg_parser.add_argument("--model_path", type=str, default="./Data/model/")
-    arg_parser.add_argument("--data_path", type=str, default='./Data/regression/phosphatase/phosphatase.csv')
+    arg_parser.add_argument("--data_path", type=str, default='./Data/regression/lograte/lograte.csv')
     # arg_parser.add_argument("--column_rxn", type=str, default="AAM") #ver3,4,5
     # arg_parser.add_argument("--reaction_mode_str", type=str, default="reac_diff") #mới cho ver4   
-    arg_parser.add_argument("--graph_path", type=str, default='./Data/regression/phosphatase/its_origin/phosphatase.pkl.gz') #binh thuong bo cho nay
+    arg_parser.add_argument("--graph_path", type=str, default='./Data/regression/lograte/its_origin/lograte.pkl.gz') #binh thuong bo cho nay
     arg_parser.add_argument("--model_name", type=str, default="model.pt")
-    arg_parser.add_argument("--y_column", type=str, default="Conversion")
+    arg_parser.add_argument("--y_column", type=str, default="lograte")
     arg_parser.add_argument("--seed", type=int, default=27407)
     args = arg_parser.parse_args()
 
