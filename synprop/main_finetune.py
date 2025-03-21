d5fdaf03d9118454f98ba7032c79983731fd05ae
root <root@DESKTOP-HN5ET2A.>
2025-03-21 17:00:58 +0000

    updated from local

--- synprop/main_finetune.py
+++ synprop/main_finetune.py
@@ -10,7 +10,7 @@ import sys
 root_dir=Path(__file__).resolve().parents[1]
 sys.path.append(str(root_dir))
 os.chdir(str(root_dir))
-from synprop.finetune_graph_GIN import finetune
+from synprop.finetune_graph_GCN import finetune
 
 
 if __name__ == "__main__":
@@ -22,12 +22,12 @@ if __name__ == "__main__":
     arg_parser.add_argument("--monitor_name", type=str, default="monitor.txt")
     arg_parser.add_argument("--Data_folder", type=str, default="./Data/")
     arg_parser.add_argument("--model_path", type=str, default="./Data/model/")
-    arg_parser.add_argument("--data_path", type=str, default='./Data/regression/lograte/lograte.csv')
+    arg_parser.add_argument("--data_path", type=str, default='./Data/regression/rad6re/rad6re.csv')
     # arg_parser.add_argument("--column_rxn", type=str, default="AAM") #ver3,4,5
     # arg_parser.add_argument("--reaction_mode_str", type=str, default="reac_diff") #mới cho ver4   
-    arg_parser.add_argument("--graph_path", type=str, default='./Data/regression/lograte/its_origin/lograte.pkl.gz') #binh thuong bo cho nay
+    arg_parser.add_argument("--graph_path", type=str, default='./Data/regression/rad6re/its_origin/rad6re.pkl.gz') #binh thuong bo cho nay
     arg_parser.add_argument("--model_name", type=str, default="model.pt")
-    arg_parser.add_argument("--y_column", type=str, default="lograte")
+    arg_parser.add_argument("--y_column", type=str, default="dh")
     arg_parser.add_argument("--seed", type=int, default=27407)
     args = arg_parser.parse_args()
 
