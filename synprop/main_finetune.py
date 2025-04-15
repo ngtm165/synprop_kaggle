c3bbbd49becc09b4360bfcb625a25d3744d5c20b
root <root@DESKTOP-HN5ET2A.>
2025-04-15 13:49:13 +0000

    updated from local

--- synprop/main_finetune.py
+++ synprop/main_finetune.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     # arg_parser.add_argument("--reaction_mode_str", type=str, default="reac_diff") #mới cho ver4   
     arg_parser.add_argument("--graph_path", type=str, default='./Data/regression/wb97xd3/its_new/wb97xd3.pkl.gz') #binh thuong bo cho nay
     arg_parser.add_argument("--model_name", type=str, default="model.pt")
-    arg_parser.add_argument("--y_column", type=str, default="ea")
+    arg_parser.add_argument("--y_column", type=str, default="dh")
     arg_parser.add_argument("--seed", type=int, default=27407)
     args = arg_parser.parse_args()
 
