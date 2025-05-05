ba20d38f34ebce894744c6da2f05b407d1156f9d
ngtm165 <ngoctamtrannguyen123@gmail.com>
2025-05-05 13:40:42 +0700

    Update gine.py

--- synprop/gine.py
+++ synprop/gine.py
@@ -9,13 +9,12 @@ class GNN(nn.Module):
         self,
         node_in_feats,
         edge_in_feats,
-        depth=5, #default = 5; opt = 3
+        depth=depth, #default = 5; opt = 3
         node_hid_feats=300, #default = 300; opt = 1700
         readout_feats=1024,
         dr=0.1, #default = 0.1; opt = 0.2
         readout_option=True,
         lr=lr, ##mới thêm
-        depth=depth, ##mới thêm
     ):
         super(GNN, self).__init__()
 
