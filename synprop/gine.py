08140d894ce1cb148822fc687f425c17573c0c37
root <root@DESKTOP-HN5ET2A.>
2025-05-05 12:26:57 +0000

    updated from local

--- synprop/gine.py
+++ synprop/gine.py
@@ -10,9 +10,9 @@ class GNN(nn.Module):
         node_in_feats,
         edge_in_feats,
         depth=6, #default = 5; opt = 3
-        node_hid_feats=300, #default = 300; opt = 1700
-        readout_feats=1536, #default = 1024
-        dr=0.2, #default = 0.1; opt = 0.2
+        node_hid_feats=128, #default = 300; opt = 1700
+        readout_feats=1024, #default = 1024
+        dr=0.25, #default = 0.1; opt = 0.2
         readout_option=True, #default = True
         # lr=lr, ##mới thêm
     ):
