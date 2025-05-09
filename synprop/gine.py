c66a3caade6a00cc648fe9c55d97fd6d3ee995a5
huy752 <huy070502@gmail.com>
2025-05-09 11:48:51 +0700

    updated from lab2

--- synprop/gine.py
+++ synprop/gine.py
@@ -9,10 +9,10 @@ class GNN(nn.Module):
         self,
         node_in_feats,
         edge_in_feats,
-        depth=4, #default = 5; opt = 3
+        depth=5, #default = 5; opt = 3
         node_hid_feats=128, #default = 300; opt = 1700
-        readout_feats=768, #default = 1024
-        dr=0.0, #default = 0.1; opt = 0.2
+        readout_feats=704, #default = 1024
+        dr=0.30000000000000004, #default = 0.1; opt = 0.2
         readout_option=True, #default = True
         # lr=lr, ##mới thêm
     ):
