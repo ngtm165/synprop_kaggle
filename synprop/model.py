914d91038a4ec9ff6cf1911f2f5120e2bbe14c85
huy752 <huy070502@gmail.com>
2025-05-09 09:58:10 +0700

    updated from lab2

--- synprop/model.py
+++ synprop/model.py
@@ -23,16 +23,16 @@ class model(nn.Module):
         edge_feat,
         out_dim=1, #default = 1
         num_layer=3, #default = 3
-        node_hid_feats=416, #default = 300
+        node_hid_feats=160, #default = 300
         readout_feats=1024, #default = 1024
-        predict_hidden_feats=448, #default = 512
+        predict_hidden_feats=256, #default = 512
         readout_option=False, #default=False
         drop_ratio=0.0, #default = 0.1
         # lr=lr, ##mới thêm
         # depth=depth, ##mới thêm
     ):
         super(model, self).__init__()
-        emb_dim=416 #default = 1024
+        emb_dim=160 #default = 1024
         self.gnn = GNN(node_feat,edge_feat)  
 
 
@@ -59,8 +59,8 @@ def train(
     val_loader,
     model_path,
     device,
-    lr=0.00020007443952154756, ##mới thêm 5e-4
-    weight_decay=2.3141979258845455e-05, ##mới thêm 1e-5
+    lr=0.0008687615395967226, ##mới thêm 5e-4
+    weight_decay=9.925575308848027e-05, ##mới thêm 1e-5
     epochs=20,
     current_epoch=0,
     best_val_loss=1e10,
@@ -69,7 +69,7 @@ def train(
     n_epochs = epochs
 
     loss_fn = torch.nn.MSELoss()
-    optimizer = Adam(net.parameters(), lr=0.00020007443952154756, weight_decay=2.3141979258845455e-05) ##default: lr=5e-4, weight_decay=1e-5, chạy main_finetune (2. 1.3023669362312975e-05;  1.1848812109693355e-06)
+    optimizer = Adam(net.parameters(), lr=0.0008687615395967226, weight_decay=9.925575308848027e-05) ##default: lr=5e-4, weight_decay=1e-5, chạy main_finetune (2. 1.3023669362312975e-05;  1.1848812109693355e-06)
     # optimizer = Adam(net.parameters(), lr=lr, weight_decay=weight_decay) ##default: lr=5e-4, weight_decay=1e-5
 
     for epoch in range(n_epochs):
