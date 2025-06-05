062143d10a29ca486b925df6668bb25294461784
ngtm165 <ngoctamtrannguyen123@gmail.com>
2025-06-05 20:37:58 +0700

    Update graphdataset_5.py

--- synprop/graphdataset_5.py
+++ synprop/graphdataset_5.py
@@ -461,9 +461,9 @@ class ReactionDataset(Dataset):
 
 def main():
     
-    data_path='./Data/regression/wb97xd3/wb97xd3.csv'
-    graph_path='./Data/regression/wb97xd3/its_new/wb97xd3.pkl.gz'
-    target='ea'
+    data_path='./Data/regression/RDB7/RDB7.csv'
+    graph_path='./Data/regression/RDB7/its_new/RDB7.pkl.gz'
+    target='dHrxn298'
     graphdata=ReactionDataset(data_path,graph_path,target)
     print(graphdata.__getitem__(8))
 
@@ -473,7 +473,7 @@ def main():
     all_edge_attrs = []
     all_node_attrs = []
     all_ys = []
-    output_npz_file = 'wb97xd3_processed_data.npz'
+    output_npz_file = 'RDB7_processed_data.npz'
 
     print("\nĐang trích xuất dữ liệu từ từng mẫu trong dataset...")
     for i in range(len(graphdata)):
